b09f7f2e65650c96c231ae3bb540fe519f42dbef
Robert Best <117873513+RobertBest30@users.noreply.github.com>
2023-02-23 19:13:49 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+/*Write a program to check whether a number is divisible by 5 and 11 or not
+ * Check to see if the numbers are divisible by 12
+ */
+
+import java.util.Scanner;
+class Main
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the Number : ");
+		int num = input.nextInt();
+		
+		if(num%5==0 && num%11==0 || num%12==0)
+			System.out.println("This Number is Divisible by 5, 11, and 12");
+		
+		else
+			System.out.println("This Number is Not Divisible by 5, 11, and 12");
+	}
+}
