25c1a1441d96b6445416447fef5d6ecefe623357
Robert Best <117873513+RobertBest30@users.noreply.github.com>
2023-03-23 10:02:52 -0400

    Add files via upload

---
+++ MidTermQuestion2 (1).cs
@@ -0,0 +1,25 @@
+﻿/* 2. Write a function that takes an integer minutes and converts it to seconds.*/
+
+using System;
+using static System.Console;
+
+namespace Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int minutes;
+            int seconds;
+
+            WriteLine("Enter Minutes: ");
+            minutes = Convert.ToInt32(ReadLine());
+
+            seconds = (minutes * 60);
+
+            WriteLine(seconds);
+            ReadKey();
+        }
+    }
+}
