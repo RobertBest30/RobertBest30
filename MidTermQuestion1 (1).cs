cb6fbc3d75aeca2ced437717edd2312d4704b509
Robert Best <117873513+RobertBest30@users.noreply.github.com>
2023-03-23 10:00:18 -0400

    Add files via upload

---
+++ MidTermQuestion1 (1).cs
@@ -0,0 +1,25 @@
+﻿/* 1.Create a function that takes two numbers as arguments and returns their sum.*/
+
+using System;
+using static System.Console;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            int sum;
+
+            WriteLine("Enter First Number: ");
+            num1 = Convert.ToInt32(ReadLine());
+            WriteLine("Enter Second Number: ");
+            num2 = Convert.ToInt32(ReadLine());
+            sum = num1 + num2;
+            WriteLine("\nSum of Number 1 and Number 2: " + sum);
+            ReadKey();
+        }
+    }
+}
